8c65bcb2ee40766f201ced8a19b8fc5dfcc53934
RadheRavi3021 <radheravi01@gmail.com>
2024-07-05 10:54:27 +0530

    project setup

---
+++ src/main/java/com/projects/surveyquestionnaire/SurveyQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.projects.surveyquestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyQuestionnaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyQuestionnaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/surveyquestionnaire/SurveyQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.surveyquestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyQuestionnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
